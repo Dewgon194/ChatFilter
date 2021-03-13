9cb93fff98f4888f1390522d4381a83e8752e64f
Josh Duggan <josh.duggan20@gmail.com>
2021-03-13 12:10:16 +0000

    initial commit

--- src/main/java/com/duggan/chatfilter/ConfigReload.java
+++ src/main/java/com/duggan/chatfilter/ConfigReload.java
@@ -5,10 +5,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class ConfigReload implements CommandExecutor {
 
     private Main main = Main.getInstance();
+    Plugin plugin = Main.getPlugin(Main.class);
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -16,6 +18,7 @@ public class ConfigReload implements CommandExecutor {
             if (sender.hasPermission("indigo.command.chatfilter")) {
                 if (args.length == 1 && args[0].equals("reload")) {
                     Main.getInstance().loadConfig();
+                    plugin.reloadConfig();
                 }
             } else {
                 sender.sendMessage(ChatColor.RED + "You do not have permission!");
